ba6012b14b0693e4e4ab6e3a20bffe3a21a93175
Rakshith R <rar@redhat.com>
2021-04-11 11:48:20 +0530

    tmp

--- internal/controller/persistentvolume/persistentvolume.go
+++ internal/controller/persistentvolume/persistentvolume.go
@@ -206,7 +206,7 @@ func (r ReconcilePersistentVolume) reconcilePV(obj runtime.Object) error {
 
 // Reconcile reconciles the PersitentVolume object and creates a new omap entries
 // for the volume.
-func (r *ReconcilePersistentVolume) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+func (r *ReconcilePersistentVolume) Reconcile(ctx context.Context, request reconcile.Request) (reconcile.Result, error) {
 	pv := &corev1.PersistentVolume{}
 	err := r.client.Get(context.TODO(), request.NamespacedName, pv)
 	if err != nil {
